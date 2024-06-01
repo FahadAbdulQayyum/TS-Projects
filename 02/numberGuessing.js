c294f140cfd6de308d3326163dab0547b0eeb556
Fahad <fahad.45854@iqra.edu.com>
2024-06-01 22:37:44 +0500

    guessingNumber-CLI created

---
+++ 02/numberGuessing.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+async function main() {
+    try {
+        while (1) {
+            let answers = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "number1",
+                    message: "Guess your number between 1 to 10:",
+                },
+            ]);
+            if (answers.number1 === "x") {
+                break;
+            }
+            else {
+                console.clear();
+                console.log("Press x to exit!");
+                compare(answers.number1);
+            }
+        }
+    }
+    catch (error) { }
+}
+const compare = (num) => {
+    let randNum = Math.random() * 10;
+    randNum = +randNum.toFixed(0);
+    num = +num;
+    if (randNum < num) {
+        console.log(chalk.redBright("You guessed a higher number."), num, randNum);
+    }
+    else if (randNum > num) {
+        console.log(chalk.redBright("You guessed a lower number."), num, randNum);
+    }
+    else {
+        console.log(chalk.greenBright("Congratulation! You guessed right number.", num, randNum));
+    }
+};
+main();
--- 02/numberGuessing.ts
+++ 02/numberGuessing.ts
@@ -1,17 +1,24 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-type GuessNumber = number;
 async function main() {
   try {
-    const answers = await inquirer.prompt([
-      {
-        type: "input",
-        name: "number1",
-        message: "Guess your number between 1 to 10:",
-      },
-    ]);
-    compare(answers.number1);
+    while (1) {
+      let answers = await inquirer.prompt([
+        {
+          type: "input",
+          name: "number1",
+          message: "Guess your number between 1 to 10:",
+        },
+      ]);
+      if (answers.number1 === "x") {
+        break;
+      } else {
+        console.clear();
+        console.log("Press x to exit!");
+        compare(answers.number1);
+      }
+    }
   } catch (error) {}
 }
 
@@ -19,20 +26,10 @@ const compare = (num: number | string) => {
   let randNum = Math.random() * 10;
   randNum = +randNum.toFixed(0);
   num = +num;
-  // if (randNum < parseFloat(num)) {
   if (randNum < num) {
-    console.log(
-      chalk.bgCyanBright("You guessed a higher number."),
-      num,
-      randNum
-    );
-    // } else if (randNum > parseFloat(num)) {
+    console.log(chalk.redBright("You guessed a higher number."), num, randNum);
   } else if (randNum > num) {
-    console.log(
-      chalk.bgCyanBright("You guessed a lower number."),
-      num,
-      randNum
-    );
+    console.log(chalk.redBright("You guessed a lower number."), num, randNum);
   } else {
     console.log(
       chalk.greenBright(
@@ -43,5 +40,4 @@ const compare = (num: number | string) => {
     );
   }
 };
-
 main();
