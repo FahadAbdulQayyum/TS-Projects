32985b5e9fb22e3f50c7f98bc4ceac983af5e30e
Fahad <fahad.45854@iqra.edu.com>
2024-06-02 16:13:22 +0500

    ATM functionalities completed

--- 03/atm.ts
+++ 03/atm.ts
@@ -1,7 +1,19 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
 import data from "./data.json" assert { type: "json" };
 
+interface dataInt {
+  name: string;
+  username: string;
+  pin: number;
+  amount: string;
+}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, "data.json");
 class ATM {
   constructor() {}
   main = async (name: string, msg: string) => {
@@ -10,6 +22,46 @@ class ATM {
     ]);
     return answers;
   };
+
+  welcomeToBank = async (userInfo: dataInt) => {
+    console.log(chalk.black("1. Check Balance.\n2. Withdraw Amount"));
+    let res = await this.main("input", "Enter your options...");
+    if (+res.input === 1) {
+      console.log(chalk.black("Your amount is: " + userInfo.amount));
+    } else {
+      let res = await this.main("input", "Amount to Withdraw: ");
+      let amount: number = +userInfo.amount.split("$")[1];
+      // amount = +res.input - amount;
+      amount = amount - +res.input;
+      userInfo.amount = "$" + amount.toString();
+      console.log(chalk.black("Your remaining amount is: " + userInfo.amount));
+      console.log("read:::", this.read());
+      let read: dataInt[] = JSON.parse(this.read());
+      let found = read.find((v) => v.username === userInfo.username);
+      let otherThanFound = read.filter((v) => v.username !== userInfo.username);
+      console.log("found:", found);
+      found = userInfo;
+      for (let v of read) {
+        console.log("vvv,", v.username === found.username);
+        if (v.username === found.username) {
+          // return { ...v, amount: found.amount };
+          // return { ...otherThanFound, ...found };
+          read = [...otherThanFound, found];
+        }
+      }
+      // let updatedData = [...read, found];
+      let updatedData = read;
+      console.log("updatedDataaaaa:", updatedData);
+      this.write(updatedData);
+    }
+  };
+  read() {
+    let dt = readFileSync(filePath, "utf8");
+    return dt;
+  }
+  async write(data: dataInt[]) {
+    await writeFileSync(filePath, JSON.stringify(data));
+  }
 }
 const atm: ATM = new ATM();
 const res = await atm.main("num1", "Enter your name:");
@@ -20,8 +72,9 @@ if (result) {
   const atm: ATM = new ATM();
   const res = await atm.main("pin", "Enter your pin:");
   const { pin } = res;
-  if (result.pin === pin) {
+  if (result.pin === +pin) {
     console.log(chalk.yellow("Welcome to DUDE Bank!"));
+    atm.welcomeToBank(result);
   } else {
     console.log(chalk.red("Sorry, Wrong Pin!"));
   }
