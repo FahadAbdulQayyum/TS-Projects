f9d56e126ce0fc0ac1fb59f69551205b03e60d72
Fahad <fahad.45854@iqra.edu.com>
2024-06-02 21:06:39 +0500

    Unnecessary comments removed in SMS Project

--- 07/sms.ts
+++ 07/sms.ts
@@ -24,11 +24,6 @@ class Counter {
 
 let counter = new Counter();
 
-// const res = await counter.inpt(
-//   "option",
-//   "Enter your choices:\n1. Enter Students Info.\n2. Enter Search Students Status.\n3. Get All Informations.\n\t"
-// );
-
 const res = await counter.inpt("list", "option", "Enter your choices:", [
   "Enter Students Info",
   "Look Students Information",
