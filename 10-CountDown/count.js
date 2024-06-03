03e48ce47b3211c9ebd80c71092c86cb2b7505d8
Fahad <fahad.45843@iqra.edu.pk>
2024-06-03 23:59:36 +0500

    count down second prepared

---
+++ 10-CountDown/count.js
@@ -0,0 +1,36 @@
+import chalk from "chalk";
+async function main() {
+    // try {
+    //   while (1) {
+    //     let answers = await inquirer.prompt([
+    //       {
+    //         type: "input",
+    //         name: "number1",
+    //         message: "Guess your number between 1 to 10:",
+    //       },
+    //     ]);
+    //     if (answers.number1 === "x") {
+    //       break;
+    //     } else {
+    countDown();
+    //       }
+    //     }
+    //   } catch (error) {}
+}
+function countDown() {
+    setInterval(() => {
+        console.clear();
+        let date = new Date();
+        let sec = 0;
+        if (date.getSeconds() < 10) {
+            sec = "0" + date.getSeconds();
+        }
+        else {
+            sec = date.getSeconds();
+        }
+        console.log(chalk.black(
+        // date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
+        date.getHours() + ":" + date.getMinutes() + ":" + sec));
+    }, 1000);
+}
+main();
---
+++ 10-CountDown/countDown.js
@@ -0,0 +1,51 @@
+import inquirer from "inquirer";
+let sec = 55;
+let min = 0;
+let hr = 0;
+async function main() {
+    try {
+        while (1) {
+            let answers = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "number1",
+                    message: "Enter the minutes:",
+                },
+            ]);
+            if (answers.number1 === "x") {
+                break;
+            }
+            else {
+                countDown(+answers.number1);
+            }
+        }
+    }
+    catch (error) { }
+}
+function countDown(min) {
+    setInterval(() => {
+        console.clear();
+        // if (sec === 0 || sec === "0") {
+        //   console.log("***");
+        //   sec = "00";
+        // }
+        if (typeof sec === "number" && sec < 10) {
+            // typeof sec === "number" && sec++;
+            sec = "0" + sec;
+        }
+        if (sec === "60" || sec === 60) {
+            sec = 0;
+            if (sec === 0) {
+                sec = "00";
+            }
+            //   min++;
+            //   if (min < 10) {
+            //     min = "0" + min;
+            //   }
+        }
+        console.log(sec + ":");
+        sec = Number(sec);
+        typeof sec === "number" && sec++;
+    }, 1000);
+}
+main();
