15651fbc9f60971ac79ed9be77c292bdb9d53fa3
Fahad <fahad.45843@iqra.edu.pk>
2024-06-09 16:34:37 +0500

    oopbank data saves and read successfully

--- 12 - OOP Bank/oopbank.js
+++ 12 - OOP Bank/oopbank.js
@@ -1,4 +1,11 @@
 import inquirer from "inquirer";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+// import bankInfo from "./bank.json" assert { type: "json" };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, "bank.json");
 class MyBank {
     constructor() { }
     async process(type = "input", name, message, choices) {
@@ -7,9 +14,33 @@ class MyBank {
         ]);
         return answers[name];
     }
+    readInformation() {
+        return JSON.parse(readFileSync(filePath, "utf8"));
+    }
+    async saveInformation(data) {
+        console.log("dataaaa", data);
+        await writeFileSync(filePath, JSON.stringify(data));
+    }
 }
 const myBank = new MyBank();
 const ans = await myBank.process(undefined, "username", "Enter your username");
 console.log("ans::", ans);
-const ansChoices = await myBank.process("list", "username", "Enter your choices", ["Withdraw Amount", "Send Amount", "Balance Inquiry", "Password Change"]);
+const ansChoices = await myBank.process("list", "username", "Enter your choices", [
+    "Enter An Applicant",
+    "Update Applicant",
+    "Balance Inquiry",
+    "Password Change",
+]);
 console.log("ans::", ansChoices);
+if (ansChoices === "Enter An Applicant") {
+    const username = await myBank.process(undefined, "username", "Enter username:");
+    const pin = await myBank.process(undefined, "pin", "Enter pin:");
+    const dob = await myBank.process(undefined, "dob", "Enter dob:");
+    let data = { username, pin, dob };
+    let arr = [];
+    let existingData = myBank.readInformation();
+    // console.log("existing Data: " + existingData);
+    // arr.push(data);
+    arr.push(...existingData, data);
+    myBank.saveInformation(arr);
+}
--- 12 - OOP Bank/oopbank.ts
+++ 12 - OOP Bank/oopbank.ts
@@ -1,6 +1,19 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
+import { readFileSync, writeFileSync } from "fs";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+// import bankInfo from "./bank.json" assert { type: "json" };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = resolve(__dirname, "bank.json");
+
+interface bankInfoInt {
+  username: string;
+  pin: string;
+  dob: string;
+}
 class MyBank {
   constructor() {}
   async process(
@@ -14,6 +27,13 @@ class MyBank {
     ]);
     return answers[name];
   }
+  readInformation() {
+    return JSON.parse(readFileSync(filePath, "utf8"));
+  }
+  async saveInformation(data: bankInfoInt[]) {
+    console.log("dataaaa", data);
+    await writeFileSync(filePath, JSON.stringify(data));
+  }
 }
 const myBank: MyBank = new MyBank();
 const ans = await myBank.process(undefined, "username", "Enter your username");
@@ -23,6 +43,29 @@ const ansChoices = await myBank.process(
   "list",
   "username",
   "Enter your choices",
-  ["Withdraw Amount", "Send Amount", "Balance Inquiry", "Password Change"]
+  [
+    "Enter An Applicant",
+    "Update Applicant",
+    "Balance Inquiry",
+    "Password Change",
+  ]
 );
+
 console.log("ans::", ansChoices);
+if (ansChoices === "Enter An Applicant") {
+  const username = await myBank.process(
+    undefined,
+    "username",
+    "Enter username:"
+  );
+  const pin = await myBank.process(undefined, "pin", "Enter pin:");
+  const dob = await myBank.process(undefined, "dob", "Enter dob:");
+  let data: bankInfoInt = { username, pin, dob };
+  let arr: bankInfoInt[] = [];
+  let existingData: bankInfoInt[] = myBank.readInformation();
+  // console.log("existing Data: " + existingData);
+
+  // arr.push(data);
+  arr.push(...existingData, data);
+  myBank.saveInformation(arr);
+}
